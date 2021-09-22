97bb654b80d4c273b3b12dbf8288299c0e26c5ff
Cleeyv <cleeyv@riseup.net>
2021-09-22 15:32:18 -0400

    Add nginx proxy config for netdata on its own subdomain

--- team2-jitsi/netdata.nix
+++ team2-jitsi/netdata.nix
@@ -2,27 +2,32 @@
 
 {
   services.netdata.enable = true;
-  networking.firewall.allowedTCPPorts = [ 19999 ];
+  # To enable monitoring notifications 
   # environment.etc."netdata/health_alarm_notify.conf".source = "./netadata_health_alarm_notify.conf";
 
-  ## Nginx upstream for reverse proxy to local netdata
-  #services.nginx.upstreams = {
-  #  netdata_gateway.servers."127.0.0.1:19999" = {};
-  #  netdata_gateway.extraConfig = "keepalive 64;";
-  #};
+  # Nginx upstream for reverse proxy to local netdata
+  services.nginx.upstreams = {
+    netdata_meet.servers."127.0.0.1:19999" = { };
+    netdata_meet.extraConfig = "keepalive 64;";
+  };
 
-  ## Nginx reverse proxy config
-  #services.nginx.virtualHosts."zt.project.zone".locations."/netdata" = {
-  #  # basicAuthFile = "./netdata_htpasswd";
-  #  proxyPass = "http://netdata_gateway";
-  #  extraConfig = ''
-  #    proxy_set_header X-Forwarded-Host $host;
-  #    proxy_set_header X-Forwarded-Server $host;
-  #    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-  #    proxy_http_version 1.1;
-  #    proxy_pass_request_headers on;
-  #    proxy_set_header Connection "keep-alive";
-  #    proxy_store off;
-  #  '';
-  #};
+  # Nginx reverse proxy config
+  services.nginx.virtualHosts."data.meet.cleeyv.tech" = {
+    enableACME = true;
+    forceSSL = true;
+    # To require login to view the dashboard
+    # basicAuthFile = "./netdata_htpasswd";
+    locations."/" = {
+      proxyPass = "http://netdata_meet";
+      extraConfig = ''
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_set_header X-Forwarded-Server $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_http_version 1.1;
+        proxy_pass_request_headers on;
+        proxy_set_header Connection "keep-alive";
+        proxy_store off;
+      '';
+    };
+  };
 }
